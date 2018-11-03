c170957694b90c1d1a7150c330af951a7494ee5d
Gregory P. Smith <greg@krypto.org>
2018-11-03 00:51:05 -0700

    Add an asm_thumb fast method to set bitmap pixels.
    This adds a set_pixel_fast "method" implemented in assembly.

--- asm_thumb/monobitmap.py
+++ asm_thumb/monobitmap.py
@@ -18,6 +18,8 @@
 
 """A monochrome bitmap to represent display frame buffers."""
 
+import array
+
 
 class MonoBitmap:
     """A monochrome bitmap stored compactly.
@@ -36,7 +38,15 @@ class MonoBitmap:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.bit_buf = bytearray(width * height // 8)
+        self._bit_buf = bytearray(width * height // 8)
+
+        # (&bit_buf, width, y)
+        self.fast_in = array.array('i', [-1, width, 0])
+        store_addr(self.fast_in, self._bit_buf)
+
+    @property
+    def bit_buf(self):
+        return self._bit_buf
 
     def set_pixel(self, x: int, y: int, value: bool) -> None:
         #assert self.width > x >= 0
@@ -50,3 +60,45 @@ class MonoBitmap:
             self.bit_buf[byte_idx] |= (1 << bit_no)
         else:
             self.bit_buf[byte_idx] &= ~(1 << bit_no)
+
+    # r0 must be the self.fast_in array (&bit_buf, width, y)
+    # r1 is the x coordinate
+    # r2 is the value to set for the pixel
+    # No bounds checking is performed!
+    @staticmethod
+    @micropython.asm_thumb
+    def set_pixel_fast(r0, r1, r2):
+        # r0 <- inputs array
+        # r1 <- x
+        # r2 <- value
+        ldr(r3, [r0, 0])  # r3 <- address of bit_buf
+        ldr(r5, [r0, 4])  # r5 <- width
+        ldr(r6, [r0, 8])  # r6 <- y
+        mul(r5, r6)  # r5 <- width * y
+        add(r4, r1, r5)  # r4 <- binary_idx (width * y + x)
+        mov(r5, r4)  # r5 <- binary_idx
+        mov(r7, 3)
+        lsr(r4, r7)  # r4 <- byte_idx
+        mov(r7, 7)
+        and_(r5, r7)
+        sub(r5, r7, r5)  # r5 <- bit_no
+        mov(r6, 1)
+        lsl(r6, r5)  # r6 <- (1 << bit_no)
+        add(r3, r3, r4)  # r1 <- &bit_buf[byte_idx]
+        ldrb(r4, [r3, 0])  # r4 <- bit_buf[byte_idx]
+        mov(r7, 1)
+        and_(r2, r7)  # r2 <- value &= 1  (sets the eq zero flag)
+        beq(CLEAR_BIT)
+        orr(r4, r6)  # r4 |= (1 << bit_no)
+        b(STORE)
+        label(CLEAR_BIT)
+        bic(r4, r6)  # r4 &= ~(1 << bit_no)  # Nice instruction!
+        label(STORE)
+        strb(r4, [r3, 0])
+
+
+# This utility function really belongs somewhere else.
+@micropython.asm_thumb
+def store_addr(r0, r1):
+    """Store r1 into r0[0].  Good for getting &thing into a buffer."""
+    str(r1, [r0, 0])
