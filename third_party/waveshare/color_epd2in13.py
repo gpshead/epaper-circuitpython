b4dea4a9f46c05d3a92e56871f81173dd4dc683b
Gregory P. Smith <greg@krypto.org>
2018-11-21 00:02:35 -0800

    [untested] Port color code to CircuitPython.

--- third_party/waveshare/color_epd2in13.py
+++ third_party/waveshare/color_epd2in13.py
@@ -1,3 +1,5 @@
+# Ported to CircuitPython by Gregory P. Smith
+
 ##
  #  @filename   :   epd2in13b.py
  #  @brief      :   Implements for Dual-color e-paper library
@@ -25,14 +27,6 @@
  #
 
 import epdif
-import Image
-import ImageDraw
-import ImageFont
-import RPi.GPIO as GPIO
-
-# Display resolution
-EPD_WIDTH       = 104
-EPD_HEIGHT      = 212
 
 # EPD2IN13B commands
 PANEL_SETTING                               = 0x00
@@ -43,10 +37,10 @@ POWER_ON                                    = 0x04
 POWER_ON_MEASURE                            = 0x05
 BOOSTER_SOFT_START                          = 0x06
 DEEP_SLEEP                                  = 0x07
-DATA_START_TRANSMISSION_1                   = 0x10
+DATA_START_TRANSMISSION_1                   = 0x10  # B&W data
 DATA_STOP                                   = 0x11
 DISPLAY_REFRESH                             = 0x12
-DATA_START_TRANSMISSION_2                   = 0x13
+DATA_START_TRANSMISSION_2                   = 0x13  # tinted data
 VCOM_LUT                                    = 0x20
 W2W_LUT                                     = 0x21
 B2W_LUT                                     = 0x22
@@ -73,183 +67,123 @@ ACTIVE_PROGRAM                              = 0xA1
 READ_OTP_DATA                               = 0xA2
 POWER_SAVING                                = 0xE3
 
-# Display orientation
-ROTATE_0                                    = 0
-ROTATE_90                                   = 1
-ROTATE_180                                  = 2
-ROTATE_270                                  = 3
-
 class EPD:
+    width = 104
+    height = 212
+
     def __init__(self):
         self.reset_pin = epdif.RST_PIN
         self.dc_pin = epdif.DC_PIN
         self.busy_pin = epdif.BUSY_PIN
-        self.width = EPD_WIDTH
-        self.height = EPD_HEIGHT
-        self.rotate = ROTATE_0
 
-    def digital_write(self, pin, value):
-        epdif.epd_digital_write(pin, value)
+    def _delay_ms(self, ms):
+        time.sleep(ms / 1000.)
 
-    def digital_read(self, pin):
-        return epdif.epd_digital_read(pin)
+    def _send_command(self, command):
+        self.dc_pin.value = 0
+        epdif.spi_transfer(command.to_bytes(1, 'big'))
 
-    def delay_ms(self, delaytime):
-        epdif.epd_delay_ms(delaytime)
+    def _send_data(self, data):
+        self.dc_pin.value = 1
+        if isinstance(data, int):
+            epdif.spi_transfer(data.to_bytes(1, 'big'))
+        else:
+            # The EPD needs CS to cycle hi between every byte so we loop doing
+            # one byte transfers to cause that.  Not efficient, but it makes
+            # it work.  Data sheets say needs to be at least a 60ns CS pulse.
+            for i in range(len(data)):
+                epdif.spi_transfer(data[i:i+1])
 
-    def send_command(self, command):
-        self.digital_write(self.dc_pin, GPIO.LOW)
-        # the parameter type is list but not int
-        # so use [command] instead of command
-        epdif.spi_transfer([command])
-
-    def send_data(self, data):
-        self.digital_write(self.dc_pin, GPIO.HIGH)
-        # the parameter type is list but not int
-        # so use [data] instead of data
-        epdif.spi_transfer([data])
+    @property
+    def fb_bytes(self):
+        return self.width * self.height // 8
 
     def init(self):
-        if (epdif.epd_init() != 0):
-            return -1
+        try:
+            epdif.epd_io_bus_init()
+        except RuntimeError:
+            pass  # It avoids global io bus reinitialization.  Good.
+        self.reset_pin = epdif.RST_PIN
+        self.dc_pin = epdif.DC_PIN
+        self.busy_pin = epdif.BUSY_PIN
         self.reset()
-        self.send_command(BOOSTER_SOFT_START)
-        self.send_data (0x17)
-        self.send_data (0x17)
-        self.send_data (0x17)
-        self.send_command(POWER_ON)
+        self._send_command(BOOSTER_SOFT_START)
+        self._send_data (0x17)
+        self._send_data (0x17)
+        self._send_data (0x17)
+        self._send_command(POWER_ON)
         self.wait_until_idle()
-        self.send_command(PANEL_SETTING)
-        self.send_data(0x8F)
-        self.send_command(VCOM_AND_DATA_INTERVAL_SETTING)
-        self.send_data(0x37)
-        self.send_command(RESOLUTION_SETTING)
-        self.send_data (0x68)
-        self.send_data (0x00)
-        self.send_data (0xD4)
+        self._send_command(PANEL_SETTING)
+        self._send_data(0x8F)
+        self._send_command(VCOM_AND_DATA_INTERVAL_SETTING)
+        self._send_data(0x37)
+        self._send_command(RESOLUTION_SETTING)
+        self._send_data (0x68)
+        self._send_data (0x00)
+        self._send_data (0xD4)
 
     def wait_until_idle(self):
-        while(self.digital_read(self.busy_pin) == 0):      # 0: busy, 1: idle
-            self.delay_ms(100)
+        while self.busy_pin.value == 1:      # 0: idle, 1: busy
+            self._delay_ms(10)
 
     def reset(self):
-        self.digital_write(self.reset_pin, GPIO.LOW)         # module reset
-        self.delay_ms(200)
-        self.digital_write(self.reset_pin, GPIO.HIGH)
-        self.delay_ms(200)
+        self.reset_pin.value = 0         # module reset
+        self._delay_ms(200)
+        self.reset_pin.value = 1
+        self._delay_ms(200)
 
-    def get_frame_buffer(self, image):
-        buf = [0xFF] * (self.width * self.height / 8)
-        # Set buffer to value of Python Imaging Library image.
-        # Image must be in mode 1.
-        image_monocolor = image.convert('1')
-        imwidth, imheight = image_monocolor.size
-        if imwidth != self.width or imheight != self.height:
-            raise ValueError('Image must be same dimensions as display \
-                ({0}x{1}).' .format(self.width, self.height))
+    # TODO An API that takes a two bits per pixel image input would be good.
 
-        pixels = image_monocolor.load()
-        for y in range(self.height):
-            for x in range(self.width):
-                # Set the bits for the column of pixels at the current position.
-                if pixels[x, y] == 0:
-                    buf[(x + y * self.width) / 8] &= ~(0x80 >> (x % 8))
-        return buf
-
-    def display_frame(self, frame_buffer_black, frame_buffer_red):
-        if (frame_buffer_black != None):
-            self.send_command(DATA_START_TRANSMISSION_1)           
+    def display_frames(self, frame_buffer_black, frame_buffer_red):
+        # TODO: assert buffer lengths and _send_data all of it w/o loop?
+        if frame_buffer_black:
+            self._send_command(DATA_START_TRANSMISSION_1)           
             self.delay_ms(2)
             for i in range(0, self.width * self.height / 8):
-                self.send_data(frame_buffer_black[i])  
+                self._send_data(frame_buffer_black[i])  
             self.delay_ms(2)                  
-        if (frame_buffer_red != None):
-            self.send_command(DATA_START_TRANSMISSION_2)
+        if frame_buffer_red:
+            self._send_command(DATA_START_TRANSMISSION_2)
             self.delay_ms(2)
             for i in range(0, self.width * self.height / 8):
-                self.send_data(frame_buffer_red[i])  
+                self._send_data(frame_buffer_red[i])  
             self.delay_ms(2)        
 
-        self.send_command(DISPLAY_REFRESH)
+        self._send_command(DISPLAY_REFRESH)
         self.wait_until_idle()
 
     # after this, call epd.init() to awaken the module
     def sleep(self):
-        self.send_command(VCOM_AND_DATA_INTERVAL_SETTING)
-        self.send_data(0x37)
-        self.send_command(VCM_DC_SETTING_REGISTER)         #to solve Vcom drop 
-        self.send_data(0x00)        
-        self.send_command(POWER_SETTING)         #power setting      
-        self.send_data(0x02)        #gate switch to external
-        self.send_data(0x00)
-        self.send_data(0x00) 
-        self.send_data(0x00) 
-        self.wait_until_idle()
-        self.send_command(POWER_OFF)         #power off
+        self._send_command(VCOM_AND_DATA_INTERVAL_SETTING)
+        self._send_data(0x37)
+        self._send_command(VCM_DC_SETTING_REGISTER)         #to solve Vcom drop 
+        self._send_data(0x00)        
+        self._send_command(POWER_SETTING)         #power setting      
+        self._send_data(0x02)        #gate switch to external
+        self._send_data(b'\x00\x00\x00')
+        self._wait_until_idle()
+        self._send_command(POWER_OFF)         #power off
 
-    def set_rotate(self, rotate):
-        if (rotate == ROTATE_0):
-            self.rotate = ROTATE_0
-            self.width = epdif.EPD_WIDTH
-            self.height = epdif.EPD_HEIGHT
-        elif (rotate == ROTATE_90): 
-            self.rotate = ROTATE_90
-            self.width = epdif.EPD_HEIGHT
-            self.height = epdif.EPD_WIDTH
-        elif (rotate == ROTATE_180): 
-            self.rotate = ROTATE_180
-            self.width = epdif.EPD_WIDTH
-            self.height = epdif.EPD_HEIGHT
-        elif (rotate == ROTATE_270): 
-            self.rotate = ROTATE_270
-            self.width = epdif.EPD_HEIGHT
-            self.height = epdif.EPD_WIDTH
+    # TODO: pixel and drawing functions should be put in a shared class.
+    # such as MonoBitmap...  the only actual relevance to us is set_absolute_pixel
+    # which describes how the bits are set per byte and that a color pixel is
+    # a 0 bit (1 transparent) and a black pixel is a 1 bit (0 white) in their
+    # respective frame buffers.  the color buffer is added after the b&w is drawn.
 
-    def set_pixel(self, frame_buffer, x, y, colored):
+    def _set_absolute_pixel(self, frame_buffer, x: int, y: int, tinted: bool):
+        """Set a pixel black or tinted in a bytearray frame buffer."""
         if (x < 0 or x >= self.width or y < 0 or y >= self.height):
-            return
-        if (self.rotate == ROTATE_0):
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-        elif (self.rotate == ROTATE_90):
-            point_temp = x
-            x = epdif.EPD_WIDTH - y
-            y = point_temp
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-        elif (self.rotate == ROTATE_180):
-            x = epdif.EPD_WIDTH - x
-            y = epdif.EPD_HEIGHT- y
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-        elif (self.rotate == ROTATE_270):
-            point_temp = x
-            x = y
-            y = epdif.EPD_HEIGHT - point_temp
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-    
-    def set_absolute_pixel(self, frame_buffer, x, y, colored):
-        # To avoid display orientation effects
-        # use EPD_WIDTH instead of self.width
-        # use EPD_HEIGHT instead of self.height
-        if (x < 0 or x >= EPD_WIDTH or y < 0 or y >= EPD_HEIGHT):
-            return
-        if (colored):
-            frame_buffer[(x + y * EPD_WIDTH) / 8] &= ~(0x80 >> (x % 8))
+            raise ValueError('x %d, y %d out of range' % (x, y))
+        if tinted:
+            frame_buffer[(x + y * self.width) / 8] &= ~(0x80 >> (x % 8))
         else:
-            frame_buffer[(x + y * EPD_WIDTH) / 8] |= 0x80 >> (x % 8)
+            frame_buffer[(x + y * self.width) / 8] |= 0x80 >> (x % 8)
 
-    def draw_string_at(self, frame_buffer, x, y, text, font, colored):
-        image = Image.new('1', (self.width, self.height))
-        draw = ImageDraw.Draw(image)
-        draw.text((x, y), text, font = font, fill = 255)
-        # Set buffer to value of Python Imaging Library image.
-        # Image must be in mode 1.
-        pixels = image.load()
-        for y in range(self.height):
-            for x in range(self.width):
-                # Set the bits for the column of pixels at the current position.
-                if pixels[x, y] != 0:
-                    self.set_pixel(frame_buffer, x, y, colored)
+    set_pixel = _set_absolute_pixel  # I got rid of the rotation code.
 
-    def draw_line(self, frame_buffer, x0, y0, x1, y1, colored):
+    # TODO(gps): left off porting here.
+
+    def draw_line(self, frame_buffer, x0, y0, x1, y1, tinted):
         # Bresenham algorithm
         dx = abs(x1 - x0)
         sx = 1 if x0 < x1 else -1
@@ -257,7 +191,7 @@ class EPD:
         sy = 1 if y0 < y1 else -1
         err = dx + dy
         while((x0 != x1) and (y0 != y1)):
-            self.set_pixel(frame_buffer, x0, y0 , colored)
+            self.set_pixel(frame_buffer, x0, y0 , tinted)
             if (2 * err >= dy):
                 err += dy
                 x0 += sx
@@ -265,33 +199,33 @@ class EPD:
                 err += dx
                 y0 += sy
 
-    def draw_horizontal_line(self, frame_buffer, x, y, width, colored):
+    def draw_horizontal_line(self, frame_buffer, x, y, width, tinted):
         for i in range(x, x + width):
-            self.set_pixel(frame_buffer, i, y, colored)
+            self.set_pixel(frame_buffer, i, y, tinted)
 
-    def draw_vertical_line(self, frame_buffer, x, y, height, colored):
+    def draw_vertical_line(self, frame_buffer, x, y, height, tinted):
         for i in range(y, y + height):
-            self.set_pixel(frame_buffer, x, i, colored)
+            self.set_pixel(frame_buffer, x, i, tinted)
 
-    def draw_rectangle(self, frame_buffer, x0, y0, x1, y1, colored):
+    def draw_rectangle(self, frame_buffer, x0, y0, x1, y1, tinted):
         min_x = x0 if x1 > x0 else x1
         max_x = x1 if x1 > x0 else x0
         min_y = y0 if y1 > y0 else y1
         max_y = y1 if y1 > y0 else y0
-        self.draw_horizontal_line(frame_buffer, min_x, min_y, max_x - min_x + 1, colored)
-        self.draw_horizontal_line(frame_buffer, min_x, max_y, max_x - min_x + 1, colored)
-        self.draw_vertical_line(frame_buffer, min_x, min_y, max_y - min_y + 1, colored)
-        self.draw_vertical_line(frame_buffer, max_x, min_y, max_y - min_y + 1, colored)
+        self.draw_horizontal_line(frame_buffer, min_x, min_y, max_x - min_x + 1, tinted)
+        self.draw_horizontal_line(frame_buffer, min_x, max_y, max_x - min_x + 1, tinted)
+        self.draw_vertical_line(frame_buffer, min_x, min_y, max_y - min_y + 1, tinted)
+        self.draw_vertical_line(frame_buffer, max_x, min_y, max_y - min_y + 1, tinted)
 
-    def draw_filled_rectangle(self, frame_buffer, x0, y0, x1, y1, colored):
+    def draw_filled_rectangle(self, frame_buffer, x0, y0, x1, y1, tinted):
         min_x = x0 if x1 > x0 else x1
         max_x = x1 if x1 > x0 else x0
         min_y = y0 if y1 > y0 else y1
         max_y = y1 if y1 > y0 else y0
         for i in range(min_x, max_x + 1):
-            self.draw_vertical_line(frame_buffer, i, min_y, max_y - min_y + 1, colored)
+            self.draw_vertical_line(frame_buffer, i, min_y, max_y - min_y + 1, tinted)
 
-    def draw_circle(self, frame_buffer, x, y, radius, colored):
+    def draw_circle(self, frame_buffer, x, y, radius, tinted):
         # Bresenham algorithm
         x_pos = -radius
         y_pos = 0
@@ -299,10 +233,10 @@ class EPD:
         if (x >= self.width or y >= self.height):
             return
         while True:
-            self.set_pixel(frame_buffer, x - x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y - y_pos, colored)
-            self.set_pixel(frame_buffer, x - x_pos, y - y_pos, colored)
+            self.set_pixel(frame_buffer, x - x_pos, y + y_pos, tinted)
+            self.set_pixel(frame_buffer, x + x_pos, y + y_pos, tinted)
+            self.set_pixel(frame_buffer, x + x_pos, y - y_pos, tinted)
+            self.set_pixel(frame_buffer, x - x_pos, y - y_pos, tinted)
             e2 = err
             if (e2 <= y_pos):
                 y_pos += 1
@@ -315,7 +249,7 @@ class EPD:
             if x_pos > 0:
                 break
 
-    def draw_filled_circle(self, frame_buffer, x, y, radius, colored):
+    def draw_filled_circle(self, frame_buffer, x, y, radius, tinted):
         # Bresenham algorithm
         x_pos = -radius
         y_pos = 0
@@ -323,12 +257,12 @@ class EPD:
         if (x >= self.width or y >= self.height):
             return
         while True:
-            self.set_pixel(frame_buffer, x - x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y - y_pos, colored)
-            self.set_pixel(frame_buffer, x - x_pos, y - y_pos, colored)
-            self.draw_horizontal_line(frame_buffer, x + x_pos, y + y_pos, 2 * (-x_pos) + 1, colored)
-            self.draw_horizontal_line(frame_buffer, x + x_pos, y - y_pos, 2 * (-x_pos) + 1, colored)
+            self.set_pixel(frame_buffer, x - x_pos, y + y_pos, tinted)
+            self.set_pixel(frame_buffer, x + x_pos, y + y_pos, tinted)
+            self.set_pixel(frame_buffer, x + x_pos, y - y_pos, tinted)
+            self.set_pixel(frame_buffer, x - x_pos, y - y_pos, tinted)
+            self.draw_horizontal_line(frame_buffer, x + x_pos, y + y_pos, 2 * (-x_pos) + 1, tinted)
+            self.draw_horizontal_line(frame_buffer, x + x_pos, y - y_pos, 2 * (-x_pos) + 1, tinted)
             e2 = err
             if (e2 <= y_pos):
                 y_pos += 1
@@ -340,33 +274,3 @@ class EPD:
                 err += x_pos * 2 + 1
             if x_pos > 0:
                 break
-
-### END OF FILE ###
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
